11f7a5597a29758b2a4f2636fad433f2ca4263b9
室島祐斗 <muroshimayuuto@shitsushimayuutonoMacBook-Pro.local>
2022-05-02 11:13:12 +0900

    [finish]bl, el

---
+++ topsic_ct/topsic_ct.py
@@ -0,0 +1,8 @@
+N = int(input())
+D = [int(input()) for _ in range(1,N+1)]
+#print(D)
+
+X = sum(D[::4]) - sum(D[2::4])
+Y = sum(D[3::4]) - sum(D[1::4])
+
+print(X,Y) 
---
+++ topsic_el/topsic_el.py
@@ -0,0 +1,14 @@
+#品切れ
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+item = 0
+aaa = 0
+#print(A,B)
+for i in range(N):
+     if A[i] >= B[i]:
+         aaa = 1
+     if A[i] < B[i]:
+         item = A[i]
+print(A.index(item)+1)
