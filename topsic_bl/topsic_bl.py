7947c663fb3ed5212f076ac95abb6563617f85eb
室島祐斗 <muroshimayuuto@shitsushimayuutonoMacBook-Pro.local>
2022-05-02 10:14:33 +0900

    [finish]bl, el

--- topsic_bl/topsic_bl.py
+++ topsic_bl/topsic_bl.py
@@ -1,4 +1,4 @@
-S = list(map(str,input()))
+#S = list(map(str,input()))
 #A =[0,0,0,0,0,0,0,0,0,0]
 #B = ["a","s","d","f","g","h","j","k","l"]
 
@@ -9,7 +9,7 @@ S = list(map(str,input()))
 
 #kazu = len(S)
 
-print = (S)
+#print = (S)
 #for i in range(1, kazu):
  #   tile[i] = min(tile[i-1] + 1)
 #print(tile[-1])
@@ -35,3 +35,78 @@ print = (S)
   #          A[i][j] += A[i-1][j+1]
 
 #print(A[3][3]+1)
+
+
+#S = input()
+#kazu = len(S)
+
+#for i in range(kazu):
+ #    if kazu[i] == "a":
+  #       kazu[i] = 1
+   #  elif kazu[i] == "A":
+    #     kazu[i] = 1
+
+     #elif kazu[i] == "s":
+      #   kazu[i] =2
+     #elif kazu[i] == "S":
+      #   kazu[i] = 2
+
+     #elif kazu[i] == "d":
+      #   kazu[i] = 3
+     #elif kazu[i] == "D":
+      #   kazu[i] = 3
+
+    # elif kazu[i] == "f":
+    #     kazu[i] = 4
+    # elif kazu[i] == "F":
+    #     kazu[i] = 4
+    
+    # elif kazu[i] == "g":
+    #     kazu[i] = 5
+    # elif kazu[i] == "G":
+    #     kazu[i] = 5
+
+   #  elif kazu[i] == "h":
+    #     kazu[i] = 6
+  #   elif kazu[i] == "H":
+   #      kazu[i] = 6
+    
+    # elif kazu[i] == "j":
+    #     kazu[i] = 7
+   #  elif kazu[i] == "J":
+    #     kazu[i] = 7
+    
+   #  elif kazu[i] == "k":
+    #     kazu[i] = 8
+   #  elif kazu[i] == "K":
+   #      kazu[i] = 8
+
+    # elif kazu[i] == "l":
+    #     kazu[i] = 9
+   #  elif kazu[i] == "L":
+    #     kazu[i] = 9
+   #  else:
+    #     print("No")
+
+#aaa=kazu[0]
+
+#for j in range(1, len(kazu)):
+ #   aaa += abs(kazu[j] - kazu[i]) + 1
+
+#print(aaa) 
+
+
+
+word=list(input())
+klist=list("asdfghjkl")
+pos=0
+#総秒数
+ppos = 0
+#前の場所
+mov=0
+#移動量
+for i in word:
+    mov=abs(klist.index(i)-ppos)
+    pos=pos+mov+1
+    ppos=klist.index(i)
+print(pos)
