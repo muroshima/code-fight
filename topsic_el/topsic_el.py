28a4592f8f543a6104b470927ec4bccd16cd419f
室島祐斗 <muroshimayuuto@shitsushimayuutonoMacBook-Pro.local>
2022-05-06 09:36:10 +0900

    “改訂版2”

--- topsic_el/topsic_el.py
+++ topsic_el/topsic_el.py
@@ -4,12 +4,16 @@ A = list(map(int,input().split()))
 B = list(map(int,input().split()))
 
 item = 0
-aaa = 0
 #print(A,B)
 
 for i in range(N):
-     if A[i] > B[i]:
-         aaa = 1
+
      if A[i] < B[i]:
          item = A[i]
 print(A.index(item)+1)  
+
+
+
+
+    # if A[i] >= B[i]:
+     #    aaa = 1
